bcae94ac1c844e1418faa553a8a9780c3d736a47
joushvak17 <kamble.joushva@gmail.com>
2025-05-08 18:49:00 -0400

    Add the descriptor calculation code into the model development script file

--- scripts/model_dev.py
+++ scripts/model_dev.py
@@ -1,6 +1,63 @@
 import pandas as pd
 from chembl_webresource_client.new_client import new_client
 
+from rdkit import Chem
+from rdkit.Chem import (
+    rdMolDescriptors as rdmd,
+    GraphDescriptors,
+    Descriptors,
+    FindMolChiralCenters,
+)
+
+
+def descriptors(smiles_list: list[str]) -> pd.DataFrame:
+    """Calculates various molecular descriptors for a list of SMILES strings using RDKit.
+
+    Args:
+        smiles_list (list[str]): A list of SMILES strings representing the molecules.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the calculated molecular descriptors.
+    """
+    mol_list = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
+
+    wiener_res = []
+    amat_list = [Chem.GetDistanceMatrix(mol) for mol in mol_list]
+    for i, mol in enumerate(mol_list):
+        res = 0
+        amat = amat_list[i]
+        num_atoms = mol.GetNumAtoms()
+        for j in range(num_atoms):
+            for k in range(j + 1, num_atoms):
+                res += amat[j][k]
+        wiener_res.append(res)
+
+    descriptor_data = {
+        "Molecular Weight": [Descriptors.ExactMolWt(mol) for mol in mol_list],
+        "Number of Rotatable Bonds": [
+            Descriptors.NumRotatableBonds(mol) for mol in mol_list
+        ],
+        "Number of Atoms": [mol.GetNumAtoms() for mol in mol_list],
+        "Number of Bonds": [mol.GetNumBonds() for mol in mol_list],
+        "Count of Chiral Centers": [
+            len(FindMolChiralCenters(mol, includeUnassigned=True)) for mol in mol_list
+        ],
+        "Number of Rings": [rdmd.CalcNumRings(mol) for mol in mol_list],
+        "Number of Aromatic Rings": [
+            rdmd.CalcNumAromaticRings(mol) for mol in mol_list
+        ],
+        "Number of Hydrogen Bond Donors": [rdmd.CalcNumHBD(mol) for mol in mol_list],
+        "Number of Hydrogen Bond Acceptors": [rdmd.CalcNumHBA(mol) for mol in mol_list],
+        "Balaban J Index": [GraphDescriptors.BalabanJ(mol) for mol in mol_list],
+        "Wiener Index": wiener_res,
+        "LogP": [rdmd.CalcCrippenDescriptors(mol)[0] for mol in mol_list],
+        "TPSA": [rdmd.CalcTPSA(mol) for mol in mol_list],
+    }
+
+    descriptor_values = pd.DataFrame(descriptor_data)
+
+    return descriptor_values
+
 
 def main() -> None:
     """Main function to fetch, process, and train data from ChEMBL.
@@ -44,6 +101,16 @@ def main() -> None:
     activity_df["class"] = activity_df["standard_value"].apply(
         lambda x: "inactive" if x >= 1000 else "active"
     )
+
+    # Calculate descriptors
+    descriptor_df = descriptors(activity_df["canonical_smiles"].tolist())
+
+    # Concatenate the descriptors with the activity data, placing the class column last
+    insert_after_column: int = 1
+
+    df_data_before = activity_df.iloc[:, : insert_after_column + 1]
+    df_data_after = activity_df.iloc[:, insert_after_column + 1 :]
+    df = pd.concat([df_data_before, descriptor_df, df_data_after], axis=1)
 
     return None
 
