bb6b0cc5bbff5202a8ba5c322ffe671b0a516321
joushvak17 <kamble.joushva@gmail.com>
2025-05-09 11:07:15 -0400

    Transfer all the notebook code into a script and delete the notebooks folder/contents

--- scripts/model_dev.py
+++ scripts/model_dev.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import lightgbm as lgb
+import joblib
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from molfeat.trans.fp import FPVecTransformer
 from chembl_webresource_client.new_client import new_client
-
 from rdkit import Chem
 from rdkit.Chem import (
     rdMolDescriptors as rdmd,
@@ -107,10 +112,72 @@ def main() -> None:
 
     # Concatenate the descriptors with the activity data, placing the class column last
     insert_after_column: int = 1
-
     df_data_before = activity_df.iloc[:, : insert_after_column + 1]
     df_data_after = activity_df.iloc[:, insert_after_column + 1 :]
     df = pd.concat([df_data_before, descriptor_df, df_data_after], axis=1)
+
+    # Drop the "standard_value" column and start with the third column
+    df = df.drop(df[-2], axis=1, inplace=True)
+    df = df.iloc[:, 2:]
+
+    # Create the x and y data
+    X = df.drop(columns=["class"])
+    y = df["class"]
+
+    # Create the transformer and transform the data
+    smiles = X["canonical_smiles"]
+    transformer = FPVecTransformer(kind="desc2D", dtype=float)
+    features = transformer(smiles)
+    features_df = pd.DataFrame(features, index=smiles.index)
+    X = pd.concat([X, features_df], axis=1)
+
+    # Create the label encoder and transform the labels
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+
+    # Create the train and test data, splitting 80% for training and 20% for testing
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    print("Train size: " + str(X_train.shape[0]))
+    print("Test size: " + str(X_test.shape[0]))
+    print("Number of features: " + str(X_train.shape[1]))
+    print("Number of classes: " + str(len(label_encoder.classes_)))
+
+    # Create a LightGBM dataset
+    train_data = lgb.Dataset(X_train, label=y_train)
+    test_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
+
+    # Create the params
+    params = {
+        "task": "train",
+        "objective": "binary",
+        "boosting_type": "dart",
+        "data_sample_strategy": "bagging",
+        "tree_learner": "data",
+        "metric": "binary_logloss",
+        "num_leaves": 71,
+        "learning_rate": 0.3,
+        "feature_fraction": 0.9,
+    }
+
+    # Train the model
+    model = lgb.train(
+        params=params,
+        train_set=train_data,
+        num_boost_round=400,
+        valid_sets=test_data,
+    )
+
+    # Make some predictions
+    y_pred = model.predict(X_test, num_iteration=model.best_iteration)
+    y_pred_binary = [1 if x > 0.5 else 0 for x in y_pred]
+    accuracy = accuracy_score(y_test, y_pred_binary)
+    print(f"Accuracy: {accuracy}")
+
+    # Save the model
+    # transformer.to_state_yaml_file()
+    # joblib.dump(model, "model.pkl")
 
     return None
 
