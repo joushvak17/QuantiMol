1185e0d1fd53afde084c2fdd60f90f81a038acb3
joushvak17 <kamble.joushva@gmail.com>
2025-05-15 20:07:32 -0400

    Change name of the software in main application GUI

--- src/Interface.py
+++ src/Interface.py
@@ -10,14 +10,14 @@ try:
     from ctypes import windll
 
     windll.shcore.SetProcessDpiAwareness(1)
-except:
+except Exception:
     pass
 
 
 class Interface(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("EGFR QSAR Modeling Software")
+        self.title("QuantiMol")
         self.geometry("1400x900")
         self.resizable(False, False)
         self.configure(bg="#0F0F0F")
